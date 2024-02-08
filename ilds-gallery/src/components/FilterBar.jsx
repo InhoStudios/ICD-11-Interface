39423ac1cb93177d270947a623c6400a4f860359
Andy <andyshenproper@gmail.com>
2024-02-08 10:22:22 -0800

    Update filter bar and image card to process parent entities and properly search and filter results

--- ilds-gallery/src/components/FilterBar.jsx
+++ ilds-gallery/src/components/FilterBar.jsx
@@ -18,11 +18,11 @@ export default class FilterBar extends React.Component {
     }
 
     async getEntitiesFromDB() {
-        return;
-        let entities = await fetch(`${SERVER_ENDPOINT}/db_select?values=distinct e.entity_title, e.entity_id&from=ICD_Entity e, Lesion_Categories c&where=c.entity_id=e.entity_id`)
+        let entities = await fetch(`${SERVER_ENDPOINT}/db_select?values=distinct e.entity_title as entity_title, e.entity_id as entity_id&from=ICD_Entity e, Lesion_Categories c&where=c.entity_id=e.entity_id`)
             .then((data) => data.json())
             .catch((err) => console.log(err));
         this.setState({icd_entities: entities});
+        return;
     }
 
     handleUpdateImageType() {
@@ -32,16 +32,79 @@ export default class FilterBar extends React.Component {
     handleUpdateSeverity() {
 
     }
+
+    handleClearChecks() {
+        var all_checks = document.getElementById("all_diagnoses");
+        if (all_checks.checked) {
+            document.querySelectorAll('input[name="diagnoses"]').forEach(dCheck => {
+                dCheck.checked = false;
+            });
+            document.querySelectorAll(`.entity-card`).forEach(ec => {
+                ec.style.display = "block";
+            });
+        }
+    }
+
+    handleUpdateSearch(e) {
+        e.preventDefault();
+        var all_checks = document.getElementById("all_diagnoses");
+        all_checks.checked = true;
+        this.handleClearChecks();
+
+        let query = e.target.value;
+
+
+        document.querySelectorAll(`.entity-card`).forEach(ec => {
+            let title = ec.querySelector('h4').innerText || ec.querySelector('h4').textContent;
+            let id = ec.querySelector('span').innerText || ec.querySelector('span').textContent;
+            let classes = ec.classList;
+            console.log(title, id, classes);
+            if (title.toLowerCase().indexOf(query.toLowerCase()) > -1 || id.toLowerCase().indexOf(query.toLowerCase()) > -1) {
+                ec.style.display = "block";
+            } else {
+                ec.style.display = "none";
+            }
+        });
+        
+    }
     
     handleUpdateDiagnosisList(e) {
-        console.log(e.target.value);
+        var checks = document.getElementsByTagName("input");
+        var all_checks = document.getElementById("all_diagnoses");
+        var none_checked = true;
+        var checkedList = [];
+        for (var i = 0; i < checks.length; i++) {
+            var check = checks[i];
+            if (check.type === "checkbox" && check.name === "diagnoses" && check.checked) {
+                none_checked = false;
+                checkedList.push(check.value);
+            }
+        }
+
+        all_checks.checked = none_checked;
+
+        document.querySelectorAll(`.entity-card`).forEach(ec => {
+            if (none_checked) {
+                ec.style.display = "block";
+            } else {
+                ec.style.display = "none";
+                for (var entity of checkedList) {
+                    if (ec.classList.contains(entity) || none_checked) {
+                        ec.style.display = "block";
+                    }
+                }
+            }
+        });
+
+        console.log(none_checked);
+        
     }
 
     render() {
         return (
             <div className="row">
                 <div className="col-lg-3 mb-3">
-                    <input type="input" className="form-control" placeholder="Search" />
+                    <input type="input" className="form-control" placeholder="Search" onChange={this.handleUpdateSearch.bind(this)}/>
                 </div>
                 <div className="col-lg-9 mb-3">
                     <div className="dropdown px-2">
@@ -49,7 +112,7 @@ export default class FilterBar extends React.Component {
                         <div className="dropdown-content">
                             <a className="pe-3">
                                 <label>
-                                    <input type="checkbox" className="mx-2" value="Clinical"></input>
+                                    <input type="checkbox" className="mx-2" id="all_diagnoses" value="all" onChange={this.handleClearChecks} checked></input>
                                     All diagnoses
                                 </label>
                             </a>
--- ilds-gallery/src/components/ImageCard.jsx
+++ ilds-gallery/src/components/ImageCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Modal from "./Modal";
+import { SERVER_ENDPOINT } from "../utilities/Structures";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faExpandAlt } from '@fortawesome/free-solid-svg-icons'
 
@@ -9,7 +10,22 @@ export default class ImageCard extends React.Component {
         this.props = props;
         this.state = {
             show_modal: false,
+            parents_str: ''
         };
+    }
+
+    async componentDidMount() {
+        let parents = await fetch(`${SERVER_ENDPOINT}/db_select?values=distinct c.entity_id as entity_id&from=Lesion_Categories c&where=c.lesion_id='${this.props.image.lesion_id}'`)
+            .then((data) => data.json())
+            .catch((err) => console.log(err));
+        let parent_ids = [];
+        parents.forEach((parent) => {
+            parent_ids.push(parent.entity_id);
+        });
+        let parents_str = parent_ids.join(" ");
+        this.setState({
+            parents_str: parents_str
+        });
     }
 
     openModal(id) {
@@ -28,14 +44,14 @@ export default class ImageCard extends React.Component {
 
     render() {
         return (
-            <div className={this.props.colsize + " mb-2"}>
+            <div className={`${this.props.colsize} mb-2 entity-card ${this.props.image.entity_id} ${this.state.parents_str}`}>
                 <button className="btn">
                     <label>
                     <div className="card card-shadow" onClick={null}>
                         <div className="overlay-wrapper">
                             <img src={this.props.image.url} className="img-fluid crop-img" alt={this.props.image.entity_title}/>
                             <div className="open-modal">
-                                <a className="icon" onClick={(e) => {
+                                <a className="icon" href="#" onClick={(e) => {
                                     e.preventDefault();
                                     this.openModal(this.props.image.measurement_id);
                                 }}>
